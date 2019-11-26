e5e539f487079d42716152efb56fed875c1a5985
orYoffe <ragiop07@gmail.com>
2019-11-26 12:18:35 +0100

    added integration test

--- __tests__/index.js
+++ __tests__/index.js
@@ -29,4 +29,61 @@ describe("State", () => {
       expect(subscriber).toHaveBeenCalledWith(Object.keys(newState));
     });
   });
+
+  it("integration", () => {
+    const counterState = new State("counterState", { counter: 0 });
+    const userState = new State("userState", { username: "" });
+
+    let counterUpdateCount = 0;
+    function onCounterUpdate() {
+      ++counterUpdateCount;
+    }
+
+    let userUpdateCount = 0;
+    function onUserUpdate() {
+      ++userUpdateCount;
+    }
+
+    let anyUpdateCounterCount = 0;
+    let anyUpdateUserCount = 0;
+    let anyUpdateCount = 0;
+    function onUpdate(updatedKeys) {
+      ++anyUpdateCount;
+
+      if (updatedKeys.indexOf("counter") > -1) {
+        ++anyUpdateCounterCount;
+      } else if (updatedKeys.indexOf("username") > -1) {
+        ++anyUpdateUserCount;
+      }
+    }
+
+    counterState.subscribe(onCounterUpdate);
+    userState.subscribe(onUserUpdate);
+    counterState.subscribe(onUpdate);
+    userState.subscribe(onUpdate);
+
+    expect(anyUpdateCount).toEqual(0);
+    expect(anyUpdateCounterCount).toEqual(0);
+    expect(anyUpdateUserCount).toEqual(0);
+    expect(counterUpdateCount).toEqual(0);
+    expect(userUpdateCount).toEqual(0);
+
+    return counterState
+      .setState({ counter: ++counterState.state.counter })
+      .then(() => {
+        expect(anyUpdateCount).toEqual(1);
+        expect(anyUpdateCounterCount).toEqual(1);
+        expect(anyUpdateUserCount).toEqual(0);
+        expect(counterUpdateCount).toEqual(1);
+        expect(userUpdateCount).toEqual(0);
+
+        return userState.setState({ username: "John" }).then(() => {
+          expect(anyUpdateCount).toEqual(2);
+          expect(anyUpdateCounterCount).toEqual(1);
+          expect(anyUpdateUserCount).toEqual(1);
+          expect(counterUpdateCount).toEqual(1);
+          expect(userUpdateCount).toEqual(1);
+        });
+      });
+  });
 });
